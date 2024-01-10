9dd28ef16329992ba64345c166673fa91403a7d2
kawatech002 <kawanjiku94@gmail.com>
2024-01-10 11:48:10 +0300

    vompleted lab

--- server/app.py
+++ server/app.py
@@ -3,3 +3,14 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>welcome to my app</h1>'
+
+@app.route('/<username>')
+def user(username):
+    return f'<h1>profile for {username}</hi>'
+
+if __name__ =='__main__':
+    app.run(port=5555)
